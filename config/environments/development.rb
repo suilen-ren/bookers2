5a77f94ebb106b715deb1513b611c0d8e0ad5469
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-05-07 04:26:49 +0000

    installed:bootstrap,fontawesome,devise created:controllers,views,models routed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+   config.hosts << "d41b27064a464e09afa9064049e62ba0.vfs.cloud9.us-east-1.amazonaws.com"
 end
