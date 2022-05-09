376d7e384882f25d23af2bdd77a738fd8d4ebc9c
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-05-09 06:21:00 +0000

    creating post funcion

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,33 @@
 class BooksController < ApplicationController
   def index
+    @book= Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def create
+    @book=Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title ,:opinion)
   end
 end
