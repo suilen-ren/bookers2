87b11a80d252d0f370ce9fe6a14282b198125508
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-05-09 08:55:23 +0000

    In progress - Creating Show,user-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,35 @@
 class BooksController < ApplicationController
   def index
     @book= Book.new
+    @books= Book.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def new
+    @showbook = Book.find(params[:id])
     @book = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   def create
     @book=Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
   end
+
+  private
+
 end
