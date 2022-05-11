473462a5dede80492d0db0758b4f2837561e36b2
suilen-ren <98934676+suilen-ren@users.noreply.github.com>
2022-05-11 08:15:24 +0000

    テーブルのリセット、ユーザーの編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
-
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 end
